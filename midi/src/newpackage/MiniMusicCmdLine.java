3283be12f23f479f8bcd6b86f26a441e9ff95671
Artsiom_Berazavik <Artsiom_Berazavik@epbyminw3766.minsk.epam.com>
2016-09-15 12:08:28 +0300

    Added comment.for issue in plugin

--- midi/src/newpackage/MiniMusicCmdLine.java
+++ midi/src/newpackage/MiniMusicCmdLine.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//package newpackage;
 
 import javax.sound.midi.*;
 
-/**
- *
- * @author Artsiom_Berazavik
- */
 public class MiniMusicCmdLine {
     
     public static void main(String[] args) {
